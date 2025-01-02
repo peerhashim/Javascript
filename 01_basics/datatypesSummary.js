192f01e1c9609f424702c7889dc11c9f46e3c3be
Peer Hashim <137474298+peerhashim@users.noreply.github.com>
2025-01-02 10:29:02 +0000

    datatype summary

---
+++ 01_basics/datatypesSummary.js
@@ -0,0 +1,12 @@
+const heros =["shaktiman","naagraj","doga",];
+let myObj ={
+    name:"hashim",
+    age:24,
+}
+const myFunction = function(){
+
+    console.log("Hello World");
+}
+console.log(typeof myFunction);
+console.log(typeof myObj);
+
