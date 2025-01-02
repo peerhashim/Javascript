2b396b46337a146ec8666b3d04a3df0457269c1e
Peer Hashim <137474298+peerhashim@users.noreply.github.com>
2025-01-02 09:28:01 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,9 +1,14 @@
-let score = "33abc"
-console.log(typeof score);
-console.log(typeof(score));
-let valueInNum = Number(score)
-console.log(typeof valueInNum);
-console.log(valueInNum);
-let isLoggedIn =1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let score = "33abc"
+// console.log(typeof score);
+// console.log(typeof(score));
+// let valueInNum = Number(score)
+// console.log(typeof valueInNum);
+// console.log(valueInNum);
+// let isLoggedIn =1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+//*********************Operations*********************************
+let value = 3
+let negValue = -value
+//  
