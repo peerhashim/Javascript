6186a284ac56807ad546532cc3950b330ca49370
Peer Hashim <137474298+peerhashim@users.noreply.github.com>
2025-01-02 09:08:25 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,9 @@
+let score = "33abc"
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNum = Number(score)
+console.log(typeof valueInNum);
+console.log(valueInNum);
+let isLoggedIn =1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
