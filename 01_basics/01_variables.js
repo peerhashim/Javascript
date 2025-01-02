6ba6c35020d11765125801435292b523ee366d73
Peer Hashim <137474298+peerhashim@users.noreply.github.com>
2025-01-02 08:36:23 +0000

    variables and datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accountEmail = "peerhashim369@gmail.com"
+var accountPassword = "12345"
+let accountState ;
+accountCity = "Jaipur"
+//accountId = 42342345 // Not allowed
+accountEmail ="peerhashim666@gmail.com"
+accountPassword = "43443434"
+accountCity = "Srinagar"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+
+/*preffer not to use var because of issue in functional scope and block scope */
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"//treat all JS code as newer versipon
+//alert(2+2)// we are using nodejs not browser
+ let name = "hashim"
+ let age = 18
+ let isLogin = true
+ let state
+ //number > 2 to power 53
+ //bigint
+ //string =>  ""
+ //boolean => true / false
+ //null => standlone value
+//undefined =>
+//symbol => unique
+//object
+console.log(typeof undefined);//undefined
+console.log(typeof null);
